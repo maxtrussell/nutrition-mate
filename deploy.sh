5a3fb5d2d1596809c2dd4a0ce8d92f7354ceadd8
jobu1233 <maxtrussell@gmail.com>
2019-07-26 00:27:48 -0700

    tweaked deployment script

--- deploy.sh
+++ deploy.sh
@@ -2,8 +2,7 @@ git fetch origin
 git pull origin master
 sudo apt update && sudo apt upgrade
 sudo apt install python3-venv
-python3 -m venv env
+/usr/bin/python3 -m venv env
 source env/bin/activate
-pip install -r requirements.py
-/usr/bin/tmux kill-session -t prod
-/usr/bin/tmux new -s prod -d python3 server.py
+pip install -r requirements.txt
+python3 server.py
