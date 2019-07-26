c1994436d3913f10953db09f6da6c96872bfe1d7
jobu1233 <maxtrussell@gmail.com>
2019-07-26 00:17:55 -0700

    initial templating

--- server.py
+++ server.py
@@ -1,9 +1,25 @@
 from flask import Flask
+from flask import render_template
+from flask_bootstrap import Bootstrap
+
 app = Flask("Nutrition Mate")
+bootstrap = Bootstrap(app)
+
+@app.route("/database")
+def database_handler():
+    return render_template("database.html", active_page="database")
+
+@app.route("/log")
+def log_handler():
+    return render_template("log.html", active_page="log")
+
+@app.route("/weight")
+def weight_handler():
+    return render_template("weight.html", active_page="weight")
 
 @app.route("/")
-def hello():
-    return "This will be Nutrition Mate!"
+def home_handler():
+    return render_template("home.html", active_page="home")
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0")
