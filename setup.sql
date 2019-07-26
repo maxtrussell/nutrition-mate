4c0143dd4c5039342cf5ebeb8757c2ac22d3f1b8
jobu1233 <maxtrussell@gmail.com>
2019-07-26 15:31:39 -0700

    added sql setup script

---
+++ setup.sql
@@ -0,0 +1,32 @@
+CREATE DATABASE IF NOT EXISTS nm;
+GRANT ALL PRIVILEGES ON nm.* TO 'maxtrussell'@'localhost';
+USE nm;
+
+CREATE TABLE IF NOT EXISTS food (
+    name VARCHAR(255) NOT NULL,
+    fat FLOAT NOT NULL,
+    carbs FLOAT NOT NULL,
+    protein FLOAT NOT NULL,
+    alcohol FLOAT,
+    sugar FLOAT,
+    fiber FLOAT,
+    servings JSON,
+    username VARCHAR(255),
+    PRIMARY KEY (name)
+);
+
+CREATE TABLE IF NOT EXISTS food_log (
+    time TIMESTAMP NOT NULL,
+    name VARCHAR(255) NOT NULL,
+    serving VARCHAR(255) NOT NULL,
+    quantity FLOAT NOT NULL,
+    PRIMARY KEY (time)
+);
+
+CREATE TABLE IF NOT EXISTS weight_log (
+    time TIMESTAMP NOT NULL,
+    weight FLOAT NOT NULL,
+    notes VARCHAR(255),
+    PRIMARY KEY (time)
+);
+
