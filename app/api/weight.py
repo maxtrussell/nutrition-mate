aa0abac4f961747b93e5a754abadd888985da1d5
Maxwell Trussell <maxwell.trussell@salesforce.com>
2019-10-30 16:36:57 -0700

    added /api/weight POST

--- app/api/weight.py
+++ app/api/weight.py
@@ -1,8 +1,10 @@
-from flask import g, jsonify
+from datetime import datetime
+from flask import g, jsonify, request, Response
 import json
 
 from app.api import bp
 from app.api.auth import basic_auth
+from app.api.errors import error_response, bad_request
 import app.model.weight as _weight
 from app.pkg.config import Config
 from app.pkg.db import get_db
@@ -22,3 +24,25 @@ def get_weight():
     for weight in weights:
         data[weight.date] = weight.weight
     return jsonify(data)
+
+@bp.route('/api/weight', methods=['POST'])
+@basic_auth.login_required
+def post_weight():
+    dt = request.json.get('date')
+    weight = request.json.get('weight')
+    notes = request.json.get('notes', '')
+    if not dt:
+        return bad_request('date is a required field.')
+    if not weight:
+        return bad_request('weight is a required field.')
+    try:
+        w = _weight.Weight(float(weight))
+        w.date = datetime.strptime(dt, "%Y-%m-%d")
+        w.notes = notes
+        w.username = g.current_user.username
+        w.insert(get_db(config), config.db.WEIGHTS)
+        return Response('', status=201, mimetype='application/json')
+    except ValueError as e:
+        return bad_request(str(e))
+    except Exception as e:
+        return error_response(500)
