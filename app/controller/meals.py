1c2fead80b748fb43e284e8dd6c8ec9a83e541b3
Maxwell Trussell <maxwell.trussell@salesforce.com>
2019-11-18 23:31:05 -0800

    checkpoint

--- app/controller/meals.py
+++ app/controller/meals.py
@@ -19,11 +19,21 @@ from app.pkg.db import get_db
 meals_bp = Blueprint("meals_bp", __name__)
 config = Config()
 
-@meals_bp.route("/meals", methods=["GET"])
+@meals_bp.route("/meals", methods=["GET", "POST"])
 @login_required
 def meals_handler():
+    if request.method == "POST":
+        name = request.form.get("name")
+        new_meal =  _meal.Meal(name, current_user.username)
+        new_meal.insert(get_db(config), config.db.MEALS)
     query = request.args.get("query", None)
     meals = _meal.get_by_username(get_db(config), config.db.MEALS, current_user.username)
     for meal in meals:
         meal.get_ingredients(get_db(config), config.db.INGREDIENTS, config.db.FOODS)
     return render_template("meals.html", active_page="meals", query=query, meals=meals)
+
+@meals_bp.route("/meals/<id>", methods=["GET"])
+@login_required
+def meal_handler(id: int):
+    meal = _meal.get_by_id(get_db(config), config.db.MEALS, id)
+    return render_template("meal.html", meal=meal)
