7882c524b44a9e2b8cc14fbec2b685f03305aa22
Max Trussell <maxtrussell@gmail.com>
2019-11-22 10:17:21 -0800

    consolidated db backup into script

--- scripts/backup-db.sh
+++ scripts/backup-db.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
+# DB backup script, NOTE: ensure your user has access to the mysql db 'nm'
+
+# 1. backup db
+/usr/bin/mysqldump nm | gzip -c > /opt/nutrition-mate/backups/$(date +%Y-%m-%d).gz
+
+# 2. clean excess backups
 # automatically removes oldest db backups after limit is reached
 backups_dir="/opt/nutrition-mate/backups"
 backups_limit=10
@@ -10,3 +16,5 @@ if [ $num_backups -ge $backups_limit ]; then
     rm $(ls $backups_dir | sort -r | tail -n $num_to_remove)
 fi
 
+# 3. sync backups with gcp bucket
+gsutil rsync /opt/nutrition-mate/backups gs://nutrition-mate/
